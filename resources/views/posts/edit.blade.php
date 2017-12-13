7960562739729edd34a2925264138e2cd57bd18a
chikah923 <chika.hirano@leverages.jp>
2017-12-13 18:20:18 +0000

    Coding規約に則って修正(1)

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,27 +3,25 @@
 @section('title', 'Ladies Forum - update')
 
 @section('content')
-    <h1>Ladies Forum
-     <a href="/" class="header-menu">Back</a>
-    </h1> 
-    <h2>Edit Post</h2>
-       <form action = "/posts/update/{{$post->id}}" method ="post">
-       
-       {{ csrf_field() }}
 
-        <p>
-        <input type="text" name="name" placeholder="Your name" value= "{{ old('name', $post->name) }}">
-        </p>
-
-        <p>
-        <textarea name="body" placeholder="Enter comments">{{ old('body', $post->body) }}</textarea>
-        @if ($errors->has('body'))
-        <span class="error">{{ $errors->first('body') }}</span>
-        @endif
-        </p>
-
-        <p>
-        <input type="submit" value="Update">
-        </p>
-      </form>
+<h1>
+  Ladies Forum
+  <a class="header-menu" href="/">Back</a>
+</h1>
+<h2>Edit Post</h2>
+<form action="/posts/update/{{$post->id}}" method="post">
+  {{ csrf_field() }}
+  <p>
+    <input name="name" placeholder="Your name" type="text" value="{{ old('name', $post->name) }}" />
+  </p>
+  <p>
+    <textarea name="body" placeholder="Enter comments">{{ old('body', $post->body) }}</textarea>
+    @if ($errors->has('body'))
+    <span class="error">{{ $errors->first('body') }}</span>
+    @endif
+  </p>
+  <p>
+    <input type="submit" value="Update" />
+  </p>
+</form>
 @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,37 +3,37 @@
 @section('title', 'Ladies Forum')
 
 @section('content')
-    <h1>Ladies Forum</h1>
-     <form action = "{{ url('/posts') }}" method ="post">
-       {{ csrf_field() }}
+<h1>Ladies Forum</h1>
+<form action = "/posts"  method ="post">
+  {{ csrf_field() }}
+  <p>
+    <input type="text" name="name" placeholder="Your name" value= "{{ old('title') }}">
+  </p>
 
-        <p>
-        <input type="text" name="name" placeholder="Your name" value= "{{ old('title') }}">
-        </p>
-       
-        <p>
-        <textarea name="body" placeholder="Enter comments">{{ old('body') }}</textarea>
-        @if ($errors->has('body'))
-        <span class="error">{{ $errors->first('body') }}</span>
-        @endif
-        </p>
-        
-        <p>
-        <input type="submit" value="Post Comment">
-        </p>
-      </form>
+  <p>
+    <textarea name="body" placeholder="Enter comments">{{ old('body') }}</textarea>
+    @if ($errors->has('body'))
+    <span class="error">{{ $errors->first('body') }}</span>
+    @endif
+  </p>
 
-      <ul>
-         @forelse ($posts as $post)
-          <li>{{ $post->name }} [{{ $post->created_at }}]
-            <a href="/posts/del/{{ $post->id }}" class="del">[x]</a>
-            <a href="/posts/{{ $post->id }}" class="edit">[edit]</a>
-            <a href="/posts/show/{{ $post->id }}" class="edit">※</a>
-          </li>
-          <li>{!! nl2br(e($post->body)) !!}</li>
-          <hr>
-          @empty
-          <li>No posts yet</li>
-          @endforelse
-      </ul>
+  <p>
+    <input type="submit" value="Post Comment">
+  </p>
+</form>
+
+<ul>
+  @forelse ($posts as $post)
+  <li>
+    {{ $post->name }} [{{ $post->created_at }}]
+    <a class="del" href="/posts/del/{{ $post->id }}">[x]</a>
+    <a class="edit" href="/posts/{{ $post->id }}">[edit]</a>
+    <a class="edit" href="/posts/show/{{ $post->id }}">※</a>
+  </li>
+  <li>{!! nl2br(e($post->body)) !!}</li>
+  <hr />
+  @empty
+  <li>No posts yet</li>
+  @endforelse
+</ul>
 @endsection
