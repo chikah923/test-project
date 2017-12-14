27846a11c7153c387fc8a1b44e5b0565635c55a7
chikah923 <chika.hirano@leverages.jp>
2017-12-14 02:29:53 +0000

    コメント記載(1)

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -10,22 +10,45 @@ class CommentsController extends Controller
 {
     private $comment_model;
 
+    /** commentモデルをインスタンス化する
+    *
+    * @access public
+    * @param  obj $comment_model
+    * @return void
+    */
     public function __construct(CommentModel $comment_model)
     {
         $this->comment_model = $comment_model;
     }
 
+    /**  commentの新規保存
+    *
+    * @access public
+    * @param  String[] $request
+    *
+    * @param  int $id
+    *             commentに紐づくpostのidを格納
+    * @return response
+    * @todo   routingを通るように書き換えが必要
+    */
     public function store(Request $request, $id)
     {
         $this->validate($request, [
         'body'=>'required'
          ]);
-
+        /* $requestからパラメータのみ取得し$inputに格納 */
         $input = $request->all();
         $this->comment_model->createComment($input);
-        return redirect()->action('PostsController@show', $id); // routingを通るように書き換えが必要
+        return redirect()->action('PostsController@show', $id);
     }
 
+    /** 該当するcommentの削除
+    *
+    * @access public
+    * @param  int $comment
+    *             削除対象Commentのidを格納
+    * @return response
+    */
     public function destroy($comment)
     {
         $this->comment_model->deleteComment($comment);
--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -8,16 +8,34 @@ class Comment extends Model
 {
     protected $fillable = ['body', 'post_id'];
 
+   /** commentsテーブルとpostsテーブルの紐付きを設定
+    *
+    * @access public
+    * @return void
+    */
     public function post()
     {
         return $this->belongsTo('App\Model\Post');
     }
 
+    /** commentの新規保存
+    *
+    * @access public
+    * @param String[] $input
+    * @return void
+    */
     public function createComment($input)
     {
         return $this->create($input);
     }
 
+    /** 該当するcommentの削除
+    *
+    * @access public
+    * @param  int $comment
+    *         削除対象Commentのidを格納
+    * @return void
+    */
     public function deleteComment($comment)
     {
         return $this->find($comment)
