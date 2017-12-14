57b2012a55473caee7576a42c06c3e2525944b77
chikah923 <chika.hirano@leverages.jp>
2017-12-14 02:29:53 +0000

    コメント記載(1)

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,36 +10,76 @@ class PostsController extends Controller
 {
     private $post_model;
 
+    /** Postモデルをインスタンス化する
+    *
+    * @access public
+    * @param obj $post_model
+    * @return void
+    */
     public function __construct(PostModel $post_model)
     {
         $this->post_model = $post_model;
     }
 
+    /** 既存のpostsデータを全件取得する
+    *
+    * @access public
+    * @return response
+    */
     public function index()
     {
         $posts = $this->post_model->getAllPost();
         return view ('posts.index')->with('posts', $posts);
     }
 
+    /** postデータの新規保存
+    *
+    * @access public
+    * @param  String[] $request
+    * @return response
+    */
     public function store(PostRequest $request)
     {
+        /* $requestからパラメータの配列のみ取得し$inputに格納 */
         $input = $request->all();
         $this->post_model->createPost($input);
         return redirect('/');
     }
 
+    /** 該当するpostデータの削除
+    *
+    * @access public
+    * @param  int $id
+    *         Postのidを格納
+    * @return response
+    */
     public function destroy($id)
     {
         $this->post_model->deletePost($id);
         return redirect('/');
     }
 
+    /** 該当するpostデータの編集画面に移動
+    *
+    * @access public
+    * @param  int $id
+    *         Postのidを格納
+    * @return response
+    */
     public function edit($id)
     {
         $post = $this->post_model->getPostFromId($id);
         return view('posts.edit')->with('post', $post);
     }
 
+    /** 該当するpostデータの更新
+    *
+    * @access public
+    * @param  String[] $request
+    * @param  int $id
+    *         Postのidを格納
+    * @return response
+    */
     public function update(PostRequest $request, $id)
     {
         $input = $request->all();
@@ -47,6 +87,13 @@ class PostsController extends Controller
         return redirect('/');
     }
 
+    /** 該当するpostデータの表示
+    *
+    * @access public
+    * @param  int $id
+    *         Postのidを格納
+    * @return response
+    */
     public function show($id)
     {
         $post = $this->post_model->showPost($id);
--- app/Model/Post.php
+++ app/Model/Post.php
@@ -8,39 +8,80 @@ class Post extends Model
 {
     protected $fillable = ['name', 'body'];
 
+    /** commentsテーブルとpostsテーブルの紐付きを設定
+    *
+    * @access public
+    * @return void
+    */
     public function comments()
     {
         return $this->hasMany('App\Model\Comment');
     }
 
+    /** postsテーブルのレコードを全件取得
+    *
+    * @access public
+    * @return void
+    */
     public function getAllPost()
     {
         return $this->orderBy('created_at', 'desc')
                   ->get();
     }
 
+    /** postの新規保存
+    *
+    * @access public
+    * @param  String[] $input
+    * @return void
+    */
     public function createPost($input)
     {
         return $this->create($input);
     }
 
+    /** 該当するpostの削除
+    *
+    * @access public
+    * @param  int $id
+    * @return void
+    */
     public function deletePost($id)
     {
         return $this->find($id)
                   ->delete();
     }
 
+    /** 該当するpostレコードの取得
+    *
+    * @access public
+    * @param  int $id
+    * @return void
+    */
     public function getPostFromId($id)
     {
         return $this->find($id);
     }
 
+    /** 該当するpostの更新
+    *
+    * @access public
+    * @param  String[] $input
+    * @param  int $id
+    * @return void
+    */
     public function updatePost($input, $id)
     {
         return $this->find($id)
                    ->update($input);
     }
 
+    /** 該当するpostのレコードを取得
+    *
+    * @access public
+    * @param  int $id
+    * @return void
+    */
     public function showPost($id)
     {
         return $this->find($id);
